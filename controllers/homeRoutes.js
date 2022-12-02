e7898498c5cf59e96d99c68a5c14f44421f51e4b
Volantis <putzstuck@gmail.com>
2022-12-02 09:48:26 -0500

    successfully able to seed data...begin work on homepage handlebars file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
     });
 
     // Serialize data so the template can read it
-    const blog = blogData.map((blog) => blog.get({ plain: true }));
+    const blogPosts = blogData.map((blog) => blog.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
